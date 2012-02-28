3d090552b89e96ba5be5ce527f9567b493808046
elvis <elvis@dwenzi.com>
2012-02-28 23:46:24 +0800

    登录框实现从数据集获取登录方式

--- client/app/view/user/Sign.js
+++ client/app/view/user/Sign.js
@@ -2,9 +2,25 @@ Ext.define('doings.view.user.Sign', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.user_sign',
 
-    title: '用户登录',
-    store: 'LoginMethod',
-    tpl: new Ext.XTemplate('<ul><tpl for=".">',
-        '<li><a href="{url}">{name}</a></li>',
-        '</tpl></ul>')
+    initComponent: function() {
+        this.dataview = Ext.create('Ext.view.View', {
+            border: false,
+            cls: 'review-list',
+
+            autoScroll: true,
+            store: 'LoginMethod',
+            itemSelector: '.review',
+            tpl: new Ext.XTemplate('<ul><tpl for=".">',
+                '<li><a href="{url}">{name}</a></li>',
+                '</tpl></ul>')
+        });
+
+        Ext.apply(this, {
+            title: '用户登录',
+            layout: 'fit',
+            items:[this.dataview]
+        });
+        this.callParent(arguments);
+
+    }
 })
